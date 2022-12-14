531b33c67ac4dc8be4813b9e61355695540a5e43
ell-91 <cdsherman@ucdavis.edu>
2022-12-14 11:48:37 -0500

    updated filename and paths

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import ChatScreen from './src/screens/ChatScreen'
+import ChatScreen from './src/screens/ChatsScreen'
 
 export default function App() {
   return (
